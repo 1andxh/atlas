750383c8146252dde7eee818f2402b93ff04002a
andoh <solomonkingsman@gmail.com>
2026-08-14 05:02:50 +0000

    refactor domain exceptions away from http style

--- src/services/queue/domain/jobs/exceptions.py
+++ src/services/queue/domain/jobs/exceptions.py
@@ -6,19 +6,20 @@ class JobException(Exception):
 
     error_code: str = "JOB_FAILED"
 
-    def __init__(self, message: str, error_code: str | None, status: JobStatus) -> None:
+    def __init__(
+        self,
+        message: str,
+        error_code: str | None = None,
+    ) -> None:
         self.message = message
         self.error_code = error_code or self.error_code
-        self.status = status
         super().__init__(message)
 
 
 class InvalidJobState(JobException):
     """Raised when job state in invalid"""
 
-    error_code = "INVALID_JOB_STATE"
+    error_code: str = "INVALID_JOB_STATE"
 
-    def __init__(
-        self, status: JobStatus, message: str = "Cannont complete job in"
-    ) -> None:
+    def __init__(self, message: str = "Cannont complete job in current state") -> None:
         super().__init__(message)
