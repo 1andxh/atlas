30829de713d065a2f3a9c868e7bc6c55eb2b8a35
andoh <solomonkingsman@gmail.com>
2026-08-14 05:04:30 +0000

    add job creation fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,12 @@ from datetime import datetime
 
 
 @pytest.fixture()
-def make_job():
-    id = uuid.uuid4()
-    type = "some_type"
-    payload = {"create": 1}
-    status = JobStatus.PENDING
-    available_at = datetime.now()
-    worker_id = uuid.uuid4()
-
-    job = Job(
-        id=id,
-        type=type,
-        payload=payload,
-        status=status,
-        available_at=available_at,
-        worker_id=worker_id,
+def create_job():
+    return Job(
+        id=uuid.uuid4(),
+        type="some_type",
+        payload={"create": 1},
+        status=JobStatus.PENDING,
+        available_at=datetime.now(),
+        # worker_id=None,
     )
